9e5fc22c246446204045b1ef178f5b2ad25cb291
akirajnn <akirajnn@gmail.com>
2020-08-24 16:41:55 +0800

    加入專案檔案。

---
+++ WorkingWithWebview/App.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			var tabs = new TabbedPage ();
+			tabs.Children.Add (new WebPage { Title = "預約抽血宅急便"});
+			tabs.Children.Add (new WebPage { Title = "抽血號碼報你知"});
+			MainPage = tabs;
+		}
+	}
+}
---
+++ WorkingWithWebview/WebPage.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "https://www.ylh.gov.tw/ylh/service/bloodreg/index.php";
+            Content = browser;
+        }
+    }
+}
+
